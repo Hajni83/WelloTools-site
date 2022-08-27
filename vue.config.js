0f82566dcee9b34afd5d6d9f2c88fd16178b2c34
Hajni83 <maro.hajnalka@gmail.com>
2022-08-27 09:45:11 +0200

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = {
     port: 8081,
   },
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/site/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/docs/' : '/',
 };
